ea21761c1d373a41404d119ef496da93034b7aa0
CareteenL <15074806497@163.com>
2020-04-09 22:46:33 +0800

    core: imp React.children.map

--- packages/react/ReactChildren.js
+++ packages/react/ReactChildren.js
@@ -1,8 +1,52 @@
-function mapChildren(children, func, context) {
-  //TODO实现此mapChildren方法
-  return children;
+import { REACT_ELEMENT_TYPE } from '../shared/ReactSymbols';
+const SEPARATOR = '.';
+const SUBSEPARATOR = ':';
+function mapChildren(children, mapFunction, context) {
+  const result = [];
+  mapIntoWithKeyPrefixInternal(children, result, null, mapFunction, context);
+  return result;
 }
 
+function mapIntoWithKeyPrefixInternal(children, result, prefix, mapFunction, context) {
+  if (prefix != null) {
+    prefix = prefix + '/';
+  }
+  const traverseContext = { result, prefix, mapFunction, context };
+  traverseAllChildren(children, '', mapSingleChildIntoContext, traverseContext);
+}
+
+function traverseAllChildren(children, nameSoFar, mapSingleChildIntoContext, traverseContext) {
+  const type = typeof children;
+  if (type === 'string' || type === 'number' || (type === 'object' && children.$$typeof === REACT_ELEMENT_TYPE)) {
+    mapSingleChildIntoContext(
+      traverseContext,
+      children,
+      nameSoFar === '' ? SEPARATOR + getComponentKey(children, 0) : nameSoFar
+    );
+  }
+  if (Array.isArray(children)) {
+    let child;
+    let nextName;
+    const nextNamePrefix = nameSoFar === '' ? SEPARATOR : nameSoFar + SUBSEPARATOR;
+    for (let i = 0; i < children.length; i) {
+      child = children[i];
+      nextName = nextNamePrefix + getComponentKey(child, i);
+      traverseAllChildren(child, nextName, mapSingleChildIntoContext, traverseContext);
+    }
+  }
+}
+function mapSingleChildIntoContext(traverseContext, child, childKey) {
+  const { result, prefix, mapFunction, context } = traverseContext;
+  let mappedChild = mapFunction.call(context, child);
+  if (Array.isArray(mappedChild)) {
+    mapIntoWithKeyPrefixInternal(mappedChild, result, childKey, c => c);
+  } else if (mappedChild != null) {
+    result.push({ ...mappedChild, key: prefix + childKey });
+  }
+}
+function getComponentKey(component, index) {
+  return component.key || index.toString(36);
+}
 export {
-  mapChildren as map,
+  mapChildren as map
 };
