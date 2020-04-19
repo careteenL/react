eefbef5a0c70fc1805f72427280d2f97372b3223
CareteenL <15074806497@163.com>
2020-04-19 17:29:26 +0800

    core: imp dom-diff

--- packages/react/constants.js
+++ packages/react/constants.js
@@ -2,3 +2,7 @@ export const TEXT = Symbol.for('TEXT');//文本类型
 export const ELEMENT = Symbol.for('ELEMENT');//React元素类型 button span div
 export const FUNCTION_COMPONENT = Symbol.for('FUNCTION_COMPONENT');//函数数件
 export const CLASS_COMPONENT = Symbol.for('CLASS_COMPONENT');//类组件
+
+export const MOVE = 'MOVE';
+export const REMOVE = 'REMOVE';
+export const INSERT = 'INSERT';
--- packages/react/vdom.js
+++ packages/react/vdom.js
@@ -1,5 +1,7 @@
-import { TEXT, ELEMENT, CLASS_COMPONENT, FUNCTION_COMPONENT } from './constants';
-import { onlyOne, setProps, flatten } from './utils';
+import { TEXT, ELEMENT, CLASS_COMPONENT, FUNCTION_COMPONENT, INSERT, REMOVE, MOVE } from './constants';
+import { onlyOne, setProps, flatten, patchProps } from './utils';
+let updateDepth = 0;
+let diffQueue = [];//这是一个补丁包，记录了哪些节点需要删除 ，哪些节点需要添加
 export function compareTwoElements(oldRenderElement, newRenderElement) {
   oldRenderElement = onlyOne(oldRenderElement);
   newRenderElement = onlyOne(newRenderElement);
@@ -12,12 +14,171 @@ export function compareTwoElements(oldRenderElement, newRenderElement) {
     let newDOM = createDOM(newRenderElement);//如果节点类型不同，则需要创建新的DOM节点，然后把老DOM节点替换掉
     currentDOM.parentNode.replaceChild(newDOM, currentDOM);
     currentElement = newRenderElement;
-  } else {//新老节点都有，并且类型一样。 div span 就要进行dom diff 深度比较 比较他们的属性和他们的子节点，而且还要尽可能复用老节点
-    let newDOM = createDOM(newRenderElement);//如果节点类型不同，则需要创建新的DOM节点，然后把老DOM节点替换掉
-    currentDOM.parentNode.replaceChild(newDOM, currentDOM);
-    currentElement = newRenderElement;//TODO
+  } else {
+    //新老节点都有，并且类型一样。 div span 就要进行dom diff 深度比较 比较他们的属性和他们的子节点，而且还要尽可能复用老节点
+    updateElement(oldRenderElement, newRenderElement);
   }
   return currentElement;
+}
+function updateElement(oldElement, newElement) {
+  let currentDOM = oldElement.dom;//获取老的页面上真实存在的那个DOM节点
+  newElement.dom = oldElement.dom;//DOM要实现复用,就是为了复用老的DOM节点
+  if (oldElement.$$typeof === TEXT && newElement.$$typeof === TEXT) {
+    if (oldElement.content !== newElement.content)//如果说老的文本内容 和新的文本内容不一样的话
+      currentDOM.textContent = newElement.content;
+  } else if (oldElement.$$typeof === ELEMENT) {//如果是元素类型 span div p
+    // 先更新父节点的属性，再比较更新子节点
+    updateDOMProperties(currentDOM, oldElement.props, newElement.props);
+    updateChildrenElements(currentDOM, oldElement.props.children, newElement.props.children);
+    //会把newElement的props赋给oldElement.props
+    oldElement.props = newElement.props;
+  } else if (oldElement.$$typeof === FUNCTION_COMPONENT) {
+    updateFunctionComponent(oldElement, newElement);
+  } else if (oldElement.$$typeof === CLASS_COMPONENT) {
+    updateClassComponent(oldElement, newElement);
+  }
+}
+function updateChildrenElements(dom, oldChildrenElements, newChildrenElements) {
+  updateDepth++;//每进入一个新的子层级，就让updateDepth++
+  diff(dom, oldChildrenElements, newChildrenElements, diffQueue);
+  updateDepth--;//每比较完一层，返回上一级的时候，就updateDepth--
+  if (updateDepth === 0) {//updateDepth等于，就说明回到最上面一层了，整个更新对比就完事了
+    patch(diffQueue);//把收集到的差异 补丁传给patch方法进行更新
+    diffQueue.length = 0;
+  }
+}
+function patch(diffQueue) {
+  //第1步要把该删除的全部删除 MOVE REMOVE
+  let deleteMap = {};
+  let deleteChildren = [];
+  for (let i = 0; i < diffQueue.length; i++) {
+    let difference = diffQueue[i];
+    let { type, fromIndex, toIndex } = difference;
+    if (type === MOVE || type === REMOVE) {
+      let oldChildDOM = difference.parentNode.children[fromIndex];//先获取老的DOM节点
+      deleteMap[fromIndex] = oldChildDOM;//为了方便后面复用，放到map里
+      deleteChildren.push(oldChildDOM);
+    }
+  }
+  //把移动的和REMOVE全部删除 B D
+  deleteChildren.forEach(childDOM => {
+    childDOM.parentNode.removeChild(childDOM);
+  });
+  for (let i = 0; i < diffQueue.length; i++) {
+    let { type, fromIndex, toIndex, parentNode, dom } = diffQueue[i];
+    switch (type) {
+      case INSERT:
+        insertChildAt(parentNode, dom, toIndex);
+        break;
+      case MOVE:
+        insertChildAt(parentNode, deleteMap[fromIndex], toIndex);
+        break;
+      default:
+        break;
+    }
+  }
+}
+//要向index这个索引位置插入
+function insertChildAt(parentNode, newChildDOM, index) {
+  let oldChild = parentNode.children[index];//先取出这个索引位置的老的DOM节点
+  oldChild ? parentNode.insertBefore(newChildDOM, oldChild) : parentNode.appendChild(newChildDOM);
+}
+/**
+ * 先更新和移动的都是子节点
+ * 
+ * 1. 先更新父节点还是先更新子节点? 先更新的是父节点
+ * 2. 先移动父节点还是先移动子节点？ 先移动的是子节点
+ */
+function diff(parentNode, oldChildrenElements, newChildrenElements) {
+  //oldChildrenElementsMap={G,A} newChildrenElementsMap={A,G}
+  let oldChildrenElementsMap = getChildrenElementsMap(oldChildrenElements);//{A,B,C,D}
+  let newChildrenElementsMap = getNewChildrenElementsMap(oldChildrenElementsMap, newChildrenElements);
+  let lastIndex = 0;
+  //比较是深度优先的，所以先放子节的补丁，再放父节点的补丁
+  for (let i = 0; i < newChildrenElements.length; i++) {
+    let newChildElement = newChildrenElements[i];
+    if (newChildElement) {
+      let newKey = newChildElement.key || i.toString();
+      let oldChildElement = oldChildrenElementsMap[newKey];
+      if (newChildElement === oldChildElement) {//说明他们是同一个对象，是复用老节点
+        if (oldChildElement._mountIndex < lastIndex) {
+          diffQueue.push({
+            parentNode,//我要移除哪个父节点下的元素
+            type: MOVE,
+            fromIndex: oldChildElement._mountIndex,
+            toIndex: i
+          });
+        }
+        lastIndex = Math.max(oldChildElement._mountIndex, lastIndex);
+      } else {//如果新老元素不相等，是直接 插入
+        diffQueue.push({
+          parentNode,
+          type: INSERT,
+          toIndex: i,
+          dom: createDOM(newChildElement)
+        });
+      }
+      newChildElement._mountIndex = i;//更新挂载索引
+    }
+  }
+  for (let oldKey in oldChildrenElementsMap) {
+    if (!newChildrenElementsMap.hasOwnProperty(oldKey)) {
+      let oldChildElement = oldChildrenElementsMap[oldKey];
+      diffQueue.push({
+        parentNode,
+        type: REMOVE,
+        fromIndex: oldChildElement._mountIndex//3
+      });
+    }
+  }
+}
+function getNewChildrenElementsMap(oldChildrenElementsMap, newChildrenElements) {
+  let newChildrenElementsMap = {};
+  for (let i = 0; i < newChildrenElements.length; i++) {
+    let newChildElement = newChildrenElements[i];
+    if (newChildElement) {//说明新节点不为NULL
+      let newKey = newChildElement.key || i.toString();//A
+      let oldChildElement = oldChildrenElementsMap[newKey];
+      //可以复用1.key一样 2 需要类型是一样的
+      if (canDeepCompare(oldChildElement, newChildElement)) {
+        //在此处递归，更新父元素的时候，会递归更新每一个子元素
+        //compareTwoElements 内部会判断 updateElement
+        compareTwoElements(oldChildElement, newChildElement);//复用老的DOM节点，用新属性更新这个DOM节点
+        newChildrenElements[i] = oldChildElement;//复用老的虚拟DOM节点
+      }// key是新的元素的key,值是对应虚拟DOM。
+      newChildrenElementsMap[newKey] = newChildrenElements[i];
+    }
+  }
+  return newChildrenElementsMap;
+}
+function canDeepCompare(oldChildElement, newChildElement) {
+  if (!!oldChildElement && !!newChildElement) {// $$typeof ELEMENT type span div
+    return oldChildElement.type === newChildElement.type;//如果类型一样，就可以复用了，可以进行深度对比了
+  }
+  return false;
+}
+function getChildrenElementsMap(oldChildrenElements) {
+  let oldChildrenElementsMap = {};
+  for (let i = 0; i < oldChildrenElements.length; i++) {
+    let oldKey = oldChildrenElements[i].key || i.toString();
+    oldChildrenElementsMap[oldKey] = oldChildrenElements[i];
+  }
+  return oldChildrenElementsMap;
+}
+function updateClassComponent(oldElement, newElement) {
+  let componentInstance = oldElement.componentInstance;//获取老的类组件实例 
+  let updater = componentInstance.$updater;
+  let nextProps = newElement.props;//新的属性对象
+  updater.emitUpdate(nextProps);
+}
+//如果是要更新一个函数组件 1.拿 到老元素 2.重新执行函数组件拿 到新的元素 进行对比
+function updateFunctionComponent(oldElement, newElement) {
+  let oldRenderElement = oldElement.renderElement;//获取老的渲染出来的元素
+  let newRenderElement = newElement.type(newElement.props);// newElement.type=FunctionCounter
+  let currentElement = compareTwoElements(oldRenderElement, newRenderElement);
+}
+function updateDOMProperties(dom, oldProps, newProps) {
+  patchProps(dom, oldProps, newProps);
 }
 export function createDOM(element) {
   element = onlyOne(element);//为什么要这么写? children是一个数组
@@ -89,7 +250,6 @@ function createDOMChildren(parentNode, children) {
     parentNode.appendChild(childDOM);
   });
 }
-
 
 export function ReactElement($$typeof, type, key, ref, props) {
   let element = {
