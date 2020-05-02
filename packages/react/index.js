98d355a211e11eb23386a0e60b65173d3508e799
CareteenL <15074806497@163.com>
2020-05-02 13:11:19 +0800

    core: imp createContext

--- packages/react/index.js
+++ packages/react/index.js
@@ -2,9 +2,7 @@
 import { TEXT, ELEMENT, CLASS_COMPONENT, FUNCTION_COMPONENT } from './constants';
 import { ReactElement } from './vdom';
 import { Component } from './component';
-// import { Component } from './ReactBaseClasses';
-import { createElement } from './ReactElement';
-import { map } from './ReactChildren';
+import { onlyOne, flatten } from './utils';
 function createElement(type, config = {}, ...children) {
   delete config.__source;
   delete config.__self;
@@ -18,8 +16,9 @@ function createElement(type, config = {}, ...children) {
   } else if (typeof type === 'function') {
     $$typeof = FUNCTION_COMPONENT;//说明是一个函数数组
   }
+  children = flatten(children);
   props.children = children.map(item => {
-    if (typeof item === 'object') {
+    if (typeof item === 'object' || typeof item === 'function') {
       return item;// React.createElement('span', { color: 'red' }, 'Hello')
     } else {
       return { $$typeof: TEXT, type: TEXT, content: item };//item = "Hello"
@@ -27,15 +26,27 @@ function createElement(type, config = {}, ...children) {
   })
   return ReactElement($$typeof, type, key, ref, props);
 }
-
-const React = {
-  createElement,
-  Component,
-  Children: {
-    map
-  },
+function createRef() {
+  return { current: null };
 }
 export {
   Component
+}
+function createContext(defaultValue) {
+  Provider.value = defaultValue;//context会赋一个初始值
+  function Provider(props) {
+    Provider.value = props.value;//每次Provider重新更新时候，也会重新赋值
+    return props.children;
+  }
+  function Consumer(props) {
+    return onlyOne(props.children)(Provider.value);
+  }
+  return { Provider, Consumer };
+}
+const React = {
+  createElement,
+  Component,
+  createRef,
+  createContext
 }
 export default React;
