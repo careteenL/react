07dc19e000ee9237e10d14942f675148fe0320cd
CareteenL <15074806497@163.com>
2020-05-02 13:10:20 +0800

    core: imp unstable_batchedUpdates

--- packages/react-dom/index.js
+++ packages/react-dom/index.js
@@ -1,11 +1,22 @@
 
 import { createDOM } from '../react/vdom';
+import { updateQueue } from '../react/component';
 function render(element, container) {
   // 1.要把虚拟 DOM变成真实DOM
   let dom = createDOM(element);
   // 2.把直实DOM挂载到container上
   container.appendChild(dom);
 }
+function unstable_batchedUpdates(fn) {
+  updateQueue.isPending = true; // 强行处理批量更新模式
+  fn();
+  updateQueue.isPending = false;
+  updateQueue.batchUpdate();
+}
+export {
+  unstable_batchedUpdates
+}
 export default {
-  render
+  render,
+  unstable_batchedUpdates
 }
